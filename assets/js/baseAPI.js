f9f8d87180370e9e8099a994e1cbaaba7ba99b40
xuhuanGG <978873427@qq.com>
2022-06-27 17:04:52 +0800

    完成了首页

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,18 @@
+// 统一接口
+$(function() {
+    $.ajaxPrefilter(function(options) {
+        options.url = 'http://www.liulongbin.top:3007' + options.url
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = { Authorization: localStorage.getItem('token') || '' }
+        }
+        options.complete = function(res) {
+            if (res.responseJSON.status == 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1.清空token
+                localStorage.removeItem('token');
+                // 2.跳转到登录页
+                location.href = '/login.html'
+            }
+        }
+
+    })
+})
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,49 @@
 $(function() {
+    // 调用函数获取用户信息
+    getUserInfo();
+    // 
+    function getUserInfo() {
+        $.ajax({
+            url: '/my/userinfo',
+            method: 'GET',
+
+            success: function(res) {
+
+                if (res.status !== 0) {
+                    layui.layer.msg(res.message)
+                } else {
+                    renderAvatar(res.data);
+                }
+
+            }
+        })
+    }
+
+    function renderAvatar(user) {
+        var name = user.nickname || user.username;
+        $('#welcome').html('欢迎&nbsp&nbsp' + name)
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('src', user.ser_pic)
+            $('.text-avatar').hide();
+            $('.layui-nav-img').show();
+        } else {
+            $('.text-avatar').show();
+            $('.layui-nav-img').hide();
+            $('.text-avatar').html(name[0].toUpperCase())
+
+        }
+    }
+    var layer = layui.layer
+    $('#btnLogout').on('click', function() {
+        layer.confirm('确认退出登录？', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            // 1.清除token
+            localStorage.removeItem('token');
+            // 2.跳转到登录页
+            location.href = '/login.html'
+
+            layer.close(index);
+        });
+    })
 
 })
